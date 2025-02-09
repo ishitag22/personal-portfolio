d1bdebf666334a0c201b7923378f162e4475e77c
Ishita Gaur <92143975+ishitag22@users.noreply.github.com>
2025-02-09 17:59:23 +0530

    Initial commit
    Signed-off-by: Ishita Gaur <92143975+ishitag22@users.noreply.github.com>

---
+++ src/content/information.js
@@ -0,0 +1,16 @@
+//personal information start
+
+const userData = {
+  firstName: "<Ishita",
+  lastName: "Gaur/>",
+  title: "About Me",
+  // img: require("../assets/uni.png"),
+  description:
+    "Hey there! 👋 I'm Ishita Gaur, a passionate Software Engineering undergrad at Jaypee University of Engineering and Technology, Guna. 🌌 My journey spans from crafting code to delving into the realms of . Let's connect and embark on an exciting adventure through the vast universe of technology! 🚀 ",
+};
+
+//personal information end
+
+const information = { userData };
+
+export default information;
---
+++ src/content/socials.js
@@ -0,0 +1,12 @@
+const socials = [
+  {
+    icon: "github.svg",
+    url: "https://github.com/ishitag22",
+  },
+  {
+    icon: "linkedin.svg",
+    url: "https://www.linkedin.com/in/ishitag22/",
+  },
+];
+
+export default socials;
